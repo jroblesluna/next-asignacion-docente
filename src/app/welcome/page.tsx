32d2132029abd01ca3f57669a9ab2f3c6b02e7a4
Javier Robles <j@iol.pe>
2024-09-16 13:04:07 -0500

    20240916120000

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -4,11 +4,9 @@ import React from "react";
 function Page() {
   return (
     <>
-      <div>Index Root Page</div>
+      <div>Welcome Page</div>
       <div className="w-fit bg-green-300 hover:bg-green-600 border-green-800 rounded-xl p-2">
-        <Link href={{ pathname: "/welcome", query: { getInputField1: "getInputData1" } }}>
-          Go to Welcome
-        </Link>
+        <Link href="/">Go to Index</Link>
       </div>
     </>
   );
