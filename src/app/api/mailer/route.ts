649b933f7a82277d03d23601b383ba4c8a41a1b8
JoseEnriqueMendo <dinofiesta123@gmail.com>
2024-09-17 16:34:45 -0500

    structure api services

--- src/app/api/mailer/route.ts
+++ src/app/api/mailer/route.ts
@@ -1,6 +1,5 @@
-// const { id } = req.query;
 import { NextResponse } from 'next/server';
-import { connectToDatabase } from '../../../lib/db';
+import { connectToDatabase } from '../lib/db';
 
 export async function GET() {
   try {
