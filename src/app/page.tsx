c9a9b33b75015b8018b65df43ddce5b0511a3d1f
Javier Robles <j@iol.pe>
2024-09-16 23:29:11 -0500

    202409162328

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ function Page() {
   return (
     <>
       <div>Index Root Page</div>
+      <img
+          src="/icpna-icon.svg"
+          className="cursor-pointer hover:opacity-80 w-64 mt-5 max-[600px]:mx-auto"
+        />
       <div className="w-fit bg-green-300 hover:bg-green-600 border-green-800 rounded-xl p-2">
         <Link href={{ pathname: "/welcome", query: { getInputField1: "getInputData1" } }}>
           Go to Welcome
