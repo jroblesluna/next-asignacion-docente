e60e5de3050fd7d2d6392bc99fb9f20a0f563283
JoseEnriqueMendo <dinofiesta123@gmail.com>
2024-09-18 17:01:20 -0500

    test env

--- src/app/lib/msalConfig.ts
+++ src/app/lib/msalConfig.ts
@@ -4,9 +4,7 @@ const config = {
   auth: {
     clientId:
       process.env.NEXT_PUBLIC_AZURE_WEBAPP_ID_CLIENT || 'cea39403-2b37-4bde-9cae-c9246ebdd03b',
-    authority:
-      'https://login.microsoftonline.com/' + process.env.NEXT_PUBLIC_AZURE_WEBAPP_ID_TENANT ||
-      'c25f5a9d-1acb-41dc-8777-55458159b9d9',
+    authority: 'https://login.microsoftonline.com/' + 'c25f5a9d-1acb-41dc-8777-55458159b9d9',
     redirectUri: '/home',
     navigateToLoginRequestUrl: false,
     postLogoutRedirectUri: '/welcome',
