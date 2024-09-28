89b6f8c8c57ab0490ffa61009e8ec1ad37c87611
Javier Robles <j@iol.pe>
2024-09-28 00:41:30 -0500

    Test

--- src/app/services/teacher.ts
+++ src/app/services/teacher.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/teacher`;
+const BASE_URL = '/api/teacher';
 
 const teacherService = {
   getAll: async () => {
