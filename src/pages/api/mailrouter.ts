1045831a65ecf305a0173d1627e73bcca769441a
Javier Robles <j@iol.pe>
2024-09-30 09:58:55 -0500

    Minor adjustments

--- src/pages/api/mailrouter.ts
+++ src/pages/api/mailrouter.ts
@@ -21,7 +21,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const client = new EmailClient(connectionString);
 
       const emailMessage = {
-        senderAddress: "DoNotReply@notificaciones.icpna-virtual.edu.pe",
+        senderAddress: "noreply@notificaciones.icpna-virtual.edu.pe",
         content: {
           subject,
           plainText,
