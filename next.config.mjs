ec6e87d319a32447f8e75b23cf08a971b9d2ef45
JoseEnriqueMendo <dinofiesta123@gmail.com>
2024-12-02 13:00:24 -0500

    add headers

--- next.config.mjs
+++ next.config.mjs
@@ -6,25 +6,24 @@ const nextConfig = {
   },
   distDir: 'build',
   output: 'standalone',
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/(.*)',
-  //       headers: [
-  //         {
-  //           key: 'Content-Security-Policy',
-  //           value: `
-  //             default-src 'self';
-  //             script-src 'self' 'unsafe-inline' 'unsafe-eval';
-  //             style-src 'self' 'unsafe-inline';
-  //             font-src 'self' data:;
-  //             img-src 'self' data:;
-  //           `.replace(/\n/g, ''),
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://*.azureedge.net https://*.azure.com https://login.microsoftonline.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.azureedge.net; font-src 'self' https://fonts.gstatic.com https://*.azureedge.net; img-src 'self' data: https://*.azureedge.net https://*.azure.com; connect-src 'self' https://*.azure.com https://management.azure.com https://login.microsoftonline.com https://*.azureedge.net; frame-src 'self' https://login.microsoftonline.com; object-src 'none'; base-uri 'self'; form-action 'self'; report-uri /csp-violation-report-endpoint;",
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
