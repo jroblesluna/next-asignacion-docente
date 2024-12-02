3dd19743f980c4cbf1dd2d950cd9200b45d2f5ab
JoseEnriqueMendo <dinofiesta123@gmail.com>
2024-12-02 11:52:18 -0500

    fix login

--- next.config.mjs
+++ next.config.mjs
@@ -6,25 +6,25 @@ const nextConfig = {
   },
   distDir: 'build',
   output: 'standalone',
-  async headers() {
-    return [
-      {
-        source: '/(.*)', // Aplica a todas las rutas
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval';
-              style-src 'self' 'unsafe-inline';
-              font-src 'self' data:;
-              img-src 'self' data:;
-            `.replace(/\n/g, ''), // Elimina saltos de línea para evitar errores
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: `
+  //             default-src 'self';
+  //             script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  //             style-src 'self' 'unsafe-inline';
+  //             font-src 'self' data:;
+  //             img-src 'self' data:;
+  //           `.replace(/\n/g, ''),
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
