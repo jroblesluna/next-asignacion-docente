086a44de3246890c136953e5cfca7295f105f661
Javier Robles <j@iol.pe>
2024-12-03 20:31:27 -0500

    ZAP Compliance

--- next.config.mjs
+++ next.config.mjs
@@ -12,21 +12,13 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'X-Powered-By',
-            value: '', // Remove the X-Powered-By header
-          },
-          {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; script-src 'self' https://apis.google.com https://www.gstatic.com https://*.azureedge.net https://*.azure.com https://login.microsoftonline.com; style-src 'self' https://fonts.googleapis.com https://*.azureedge.net; font-src 'self' https://fonts.gstatic.com https://*.azureedge.net; img-src 'self' data: https://*.azureedge.net https://*.azure.com; connect-src 'self' https://*.azure.com https://management.azure.com https://login.microsoftonline.com https://*.azureedge.net; frame-src 'self' https://login.microsoftonline.com; object-src 'none'; base-uri 'self'; form-action 'self'; report-uri /csp-violation-report-endpoint;",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://*.azureedge.net https://*.azure.com https://login.microsoftonline.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.azureedge.net; font-src 'self' https://fonts.gstatic.com https://*.azureedge.net; img-src 'self' data: https://*.azureedge.net https://*.azure.com; connect-src 'self' https://*.azure.com https://management.azure.com https://login.microsoftonline.com https://*.azureedge.net; frame-src 'self' https://login.microsoftonline.com; object-src 'none'; base-uri 'self'; form-action 'self'; report-uri /csp-violation-report-endpoint;",
           },
           {
             key: 'Strict-Transport-Security',
             value: 'max-age=31536000; includeSubDomains; preload',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
           },
         ],
       },
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect, useState } from 'react';
 import { useMsal } from '@azure/msal-react';
 import Image from 'next/image';
