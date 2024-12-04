1823b4be93665636693cd803179d10f126b4fe05
Javier Robles <j@iol.pe>
2024-12-04 06:01:49 -0500

    Rollback Secrets

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  env: {
-    NEXT_PUBLIC_AZURE_WEBAPP_ID_CLIENT: process.env.NEXT_PUBLIC_AZURE_WEBAPP_ID_CLIENT,
-    NEXT_PUBLIC_AZURE_WEBAPP_ID_TENANT: process.env.NEXT_PUBLIC_AZURE_WEBAPP_ID_TENANT,
-  },
   distDir: 'build',
   output: 'standalone',
   async headers() {
--- src/pages/api/lib/msalEnv.ts
+++ src/pages/api/lib/msalEnv.ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const tenantId = process.env.NEXT_PUBLIC_AZURE_WEBAPP_ID_TENANT;
-    const clientId = process.env.NEXT_PUBLIC_AZURE_WEBAPP_ID_CLIENT;
+    const clientId = process.env.NEXT_PUBLIC_AZURE_WEBAPP_ID_CLIENT || 'cea39403-2b37-4bde-9cae-c9246ebdd03b';
+    const tenantId = process.env.NEXT_PUBLIC_AZURE_WEBAPP_ID_TENANT || 'c25f5a9d-1acb-41dc-8777-55458159b9d9';
     if (!clientId || !tenantId) {
         return res.status(400).json({ error: 'Faltan las configuraciones necesarias' });
     }
